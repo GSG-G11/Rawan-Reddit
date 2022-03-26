912e0dd1c09a974cdcf81a092f93af1ad3dd3a2a
rawaneyad <rawaneyad20@gmail.com>
2022-03-27 00:06:56 +0300

    Create Client Side Validation relates #9

---
+++ public/JS/signup.js
@@ -0,0 +1,99 @@
+const form = document.getElementsByTagName("form")[0];
+const userUrlImg = document.getElementById("user_url_img");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
+const userUrlImgErr = document.getElementById("userUrlImgErr");
+const usernameErr = document.getElementById("usernameErr");
+const emailErr = document.getElementById("emailErr");
+const passwordErr = document.getElementById("passwordErr");
+const confirmPasswordErr = document.getElementById("confirmPasswordErr");
+
+const displayErr = (errElem, errMsg) => {
+  errElem.innerText = errMsg;
+};
+
+const checkUserUrlImg = ()=> {
+    if (userUrlImg.validity.typeMismatch) {
+      displayErr(userUrlImgErr, "Please enter a valid URl IMAGE");
+    } else if (userUrlImg.validity.valueMissing) {
+      displayErr(userUrlImgErr, "Please enter an URl IMAGE ");
+    } else {
+      displayErr(userUrlImgErr, "");
+      return true;
+    }
+  };
+
+const checkUsername = ()=> {
+    if (username.validity.typeMismatch) {
+      displayErr(usernameErr, "Please enter a valid username");
+    } else if (username.validity.valueMissing) {
+      displayErr(usernameErr, "Please enter an username");
+    } else {
+      displayErr(usernameErr, "");
+      return true;
+    }
+  };
+
+const checkEmail = () => {
+    if (email.validity.typeMismatch) {
+      displayErr(emailErr, "Please enter a valid email");
+    } else if (email.validity.valueMissing) {
+      displayErr(emailErr, "Please enter an email");
+    } else {
+      displayErr(emailErr, "");
+      return true;
+    }
+  };
+  
+const checkPassword = () => {
+    if (password.validity.patternMismatch) {
+      displayErr(
+        passwordErr,
+        "Password must contain at least eight characters, including one letter and one number"
+      );
+    } else if (password.validity.valueMissing) {
+      displayErr(passwordErr, "Please enter a password");
+    } else {
+      displayErr(passwordErr, "");
+      return true;
+    }
+  };
+
+  const checkConfirmPassword =() =>{
+    if (password.value != confirmPassword.value) {
+      displayErr(confirmPasswordErr, "Passwords do not match");
+    } else if (confirmPassword.validity.valueMissing) {
+      displayErr(confirmPasswordErr, "Please confirm your password");
+    } else {
+      displayErr(confirmPasswordErr, "");
+      return true;
+    }
+  };
+
+  userUrlImg.addEventListener("focusout", checkUserUrlImg);
+  username.addEventListener("focusout", checkUsername);
+  email.addEventListener("focusout", checkEmail);
+  password.addEventListener("focusout", checkPassword);
+  confirmPassword.addEventListener("focusout", checkConfirmPassword);
+
+  form.addEventListener("submit", (event) => {
+    if (!checkUserUrlImg()) {
+        event.preventDefault();
+      }
+    if (!checkUsername()) {
+        event.preventDefault();
+      }
+    if (!checkEmail()) {
+      event.preventDefault();
+    }
+    if (!checkPassword()) {
+      event.preventDefault();
+    }
+    if (!checkConfirmPassword()) {
+        event.preventDefault();
+      }
+    event.preventDefault();
+  });
+  
